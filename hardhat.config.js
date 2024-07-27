31ffbb81a814629d9d177b789342c63a4c6d00ca
Zaprite <82789169+IzakVillanueva@users.noreply.github.com>
2024-07-27 15:26:43 +0800

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,44 +1,46 @@
 require("@nomicfoundation/hardhat-toolbox");
-require('dotenv').config();
 
-const FORK_FUJI = false;
-const FORK_MAINNET = false;
+const FORK_FUJI = false
+const FORK_MAINNET = false
 let forkingData = undefined;
 
 if (FORK_MAINNET) {
   forkingData = {
-    url: "https://api.avax.network/ext/bc/C/rpcc",
-  };
+    url: 'https://api.avax.network/ext/bc/C/rpcc',
+  }
 }
 if (FORK_FUJI) {
   forkingData = {
-    url: "https://api.avax-test.network/ext/bc/C/rpc",
-  };
+    url: 'https://api.avax-test.network/ext/bc/C/rpc',
+  }
 }
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.18",
+  solidity: "0.8.26",
   networks: {
     hardhat: {
       gasPrice: 225000000000,
       chainId: !forkingData ? 43112 : undefined, //Only specify a chainId if we are not forking
-      forking: forkingData,
+      forking: forkingData
     },
     fuji: {
-      url: "https://api.avax-test.network/ext/bc/C/rpc",
+      url: 'https://api.avax-test.network/ext/bc/C/rpc',
       gasPrice: 225000000000,
       chainId: 43113,
-      accounts: [process.env.WALLET_PRIVATE_KEY], // we use a .env file to hide our wallets private key
+      accounts: [
+        "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80"
+        // "YOUR PRIVATE KEY HERE"
+      ]
     },
     mainnet: {
-      url: "https://api.avax.network/ext/bc/C/rpc",
+      url: 'https://api.avax.network/ext/bc/C/rpc',
       gasPrice: 225000000000,
       chainId: 43114,
-      accounts: [process.env.WALLET_PRIVATE_KEY],
-    },
-  },
-  etherscan: {
-    apiKey: process.env.SNOWTRACE_API_KEY, // we use an .env file to hide our Snowtrace API KEY
-  },
-};
+      accounts: [
+        "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80"
+        // "YOUR PRIVATE KEY HERE"
+      ]
+    }
+  }
+}
