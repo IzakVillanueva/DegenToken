e4965daf0b5d2eec17487cdc6c66ebe48bfd1665
Zaprite <82789169+IzakVillanueva@users.noreply.github.com>
2024-07-27 19:55:03 +0800

    Update DegenToken.sol

--- contracts/DegenToken.sol
+++ contracts/DegenToken.sol
@@ -6,20 +6,17 @@ Checking token balance: Players should be able to check their token balance at a
 Burning tokens: Anyone should be able to burn tokens, that they own, that are no longer needed.*/
 
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.26;
+pragma solidity ^0.8.18;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 
-contract DegenToken is ERC20{
-    address private origOwner;
+contract DegenToken is ERC20, Ownable, ERC20Burnable{
 
-    constructor() ERC20("Degen", "DGN"){
-        origOwner = msg.sender;
-    }
+    constructor() ERC20("Degen", "DGN") Ownable(msg.sender) {}
 
-    function mintTokens(address to, uint256 amount) public {
-        require(msg.sender == origOwner, "You are not authorized to mint Degen Tokens!");
-
+    function mintTokens(address to, uint256 amount) public onlyOwner{
         _mint(to, amount);
     }
 
@@ -38,22 +35,34 @@ contract DegenToken is ERC20{
         return balanceOf(msg.sender);
     }
 
-    function browseItems() public pure returns(string memory){
-        string memory items = "Degen NFT Merch Store! 1. Official Degen NFT Jersey 2. Official Degen NFT Coin Figure 3. Official Degen NFT Cap";
-
+    function browseMerch() public pure returns(string memory items){
+        items = "Welcome to Degen Merch Store~! 1. Official Degen NFT Jersey 2. Official Degen NFT Coin Figure 3. Official Degen NFT Cap";
         return items;
     }
 
-    function redeemTokens(uint8 choice) public{
+    function redeemTokens(uint choice) public{
+        string memory merch;
+        uint256 price;
         if(choice == 1){
             require(balanceOf(msg.sender) >= 20, "You do not have enough Degen Tokens");
-            _burn(msg.sender, 20);
+            price = 20;
+            merch = "Official Degen NFT Jersey";
         } else if(choice == 2){
             require(balanceOf(msg.sender) >= 15, "You do not have enough Degen Tokens");
-            _burn(msg.sender, 15);
+            price = 15;
+            merch = "Official Degen NFT Coin Figure";
         } else if(choice == 3){
-            require(balanceOf(msg.sender) >= 12, "You do not have enough Degen Tokens");
-            _burn(msg.sender, 12);
+            require(balanceOf(msg.sender) >= 8, "You do not have enough Degen Tokens");
+            price = 8;
+            merch = "Official Degen NFT Cap";
         }
+        _burn(msg.sender, price);
+        emit RedeemedMerch(_msgSender(), choice, merch);
     }
+
+    event RedeemedMerch(
+        address indexed user,
+        uint256 indexed choice,
+        string merch
+    );
 }
